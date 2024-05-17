914b9203061eed73152362e717910ff6922a7a5a
Sedat <sedat.ates0@icloud.com>
2024-05-17 21:30:08 +0300

    folder structure

--- obj/Debug/net8.0/cron.job.test.AssemblyInfo.cs
+++ obj/Debug/net8.0/cron.job.test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cron.job.test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4473c322d302fa37612ff359888468971285f233")]
 [assembly: System.Reflection.AssemblyProductAttribute("cron.job.test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cron.job.test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
